7bb61a592f6e3fd097f5ba894cd33a5f4f8a4f6d
Клим Чистопашин <klimdrug@gmail.com>
2023-02-22 01:45:24 +0500

    Нужно больше функций

--- js/functions.js
+++ js/functions.js
@@ -31,11 +31,32 @@ extractNumber();
 
 // Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины. Символы добавляются в начало строки. Если исходная строка превышает заданную длину, она не должна обрезаться. Если «добивка» слишком длинная, она обрезается с конца.
 
+// const myPadStart = (string, minLenght, pad) => {
+//   const actualPad = minLenght - string.length;
+//   if (actualPad <= 0) {
+//     return string;
+//   }
+//   return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + string;
+// };
+// myPadStart();
+
 const myPadStart = (string, minLenght, pad) => {
-  const actualPad = minLenght - string.length;
-  if (actualPad <= 0) {
+  const howManyNeed = minLenght - string.length;
+  let resultString;
+  if (howManyNeed <= 0) {
     return string;
   }
-  return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + string;
+
+  if(pad.length > howManyNeed) {
+    return pad.slice(0, howManyNeed) + string;
+  } else {
+    resultString = (pad.repeat(howManyNeed / pad.length)) + string;
+    if(resultString.length === minLenght) {
+      return resultString;
+    }
+    resultString = pad.slice(0,resultString.length - pad.length) + resultString;
+
+    return resultString;
+  }
 };
 myPadStart();
